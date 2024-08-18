c87c715e776a85619576876033bc206181f17de8
mogilicharlahema <mogilicharlahema24@gmail.com>
2024-08-18 20:09:04 +0530

    completed

--- src/constants/data.js
+++ src/constants/data.js
@@ -3,27 +3,27 @@ import images from './images';
 const wines = [
   {
     title: 'Chapel Hill Shiraz',
-    price: '₹56',
+    price: '₹150',
     tags: 'AU | Bottle',
   },
   {
     title: 'Catena Malbee',
-    price: '₹59',
+    price: '₹190',
     tags: 'AU | Bottle',
   },
   {
     title: 'La Vieillw Rose',
-    price: '₹44',
+    price: '₹120',
     tags: 'FR | 750 ml',
   },
   {
     title: 'Rhino Pale Ale',
-    price: '₹31',
+    price: '₹135',
     tags: 'CA | 750 ml',
   },
   {
     title: 'Irish Guinness',
-    price: '₹26',
+    price: '₹215',
     tags: 'IE | 750 ml',
   },
 ];
@@ -31,27 +31,27 @@ const wines = [
 const cocktails = [
   {
     title: 'Aperol Sprtiz',
-    price: '₹20',
+    price: '₹200',
     tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
   },
   {
     title: "Dark 'N' Stormy",
-    price: '₹16',
+    price: '₹165',
     tags: 'Dark rum | Ginger beer | Slice of lime',
   },
   {
     title: 'Daiquiri',
-    price: '₹10',
+    price: '₹250',
     tags: 'Rum | Citrus juice | Sugar',
   },
   {
     title: 'Old Fashioned',
-    price: '₹31',
+    price: '₹310',
     tags: 'Bourbon | Brown sugar | Angostura Bitters',
   },
   {
     title: 'Negroni',
-    price: '₹26',
+    price: '₹265',
     tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
   },
 ];
